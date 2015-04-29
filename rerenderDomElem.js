4cbff708189d2f417742504096af59104fc89367
activenode <activenode@users.noreply.github.com>
2015-04-29 14:27:54 +0200

    Update rerenderDomElem.js

--- rerenderDomElem.js
+++ rerenderDomElem.js
@@ -1,16 +1,29 @@
-window.renderElem = function(elem, timingFunc) {
-    var sel = ('hide' in elem) ? elem[0] : elem;
-    
-    console.log(sel,'rerender here!!');
-    
-    var renderFunc = function(){
-        sel.style.display='none';
-        sel.offsetHeight; 
-        sel.style.display='';
-    };
-    if (timingFunc) {
-        timingFunc(renderFunc)
-    }
-    
-    window.setTimeout(renderFunc, 15);
-};
+(function($w,$){
+        $w.renderElem = function(elem, timingFunc, ms) {
+            var sel = ('hide' in elem) ? elem[0] : elem;
+            
+            console.warn('RENDERING NOW!');
+            
+            var ds = sel.style.display;
+            sel.style.display = 'none';
+            sel.offsetHeight;
+            sel.style.display = '';
+            
+            var render = function(){
+                $(sel).toggleClass('JM_rendertoggle');
+            };
+            
+            var timingFunc;
+            if (timingFunc==undefined) {
+                timingFunc=window.setTimeout;
+            }
+            
+            timingFunc(render, ms ? ms : 15);
+            
+            return {
+                then: function(elemThen){
+                    return $w.renderElem(elemThen, timingFunc, ms+ms);
+                }
+            };
+        };
+    }(window, jQuery));
